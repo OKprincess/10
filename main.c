6444913646f5edfd827237832401cb92c45003ba
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-11-09 15:31:43 +0900

    10_P3

--- main.c
+++ main.c
@@ -1,26 +1,19 @@
 //
 //  main.c
-//  10_P2
+//  10_P3
 //
 //  Created by ok sojoung on 2023/11/09.
 //
 
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
-    int i = 0;
-    char str[4];
-    str[0] = 'a';
-    str[1] = 'b';
-    str[2] = 'c';
-    str[3] = '\0';
+    char src[] = "The worst things to eat before you sleep";
+    char dst[100];
     
-    printf("%s\n", str);
-    
-    while(str[i] != '\0'){      // 조건이 거짓이 될 때까지 반복
-        printf("%c", str[i]);
-        i++;
-    }
+    strcpy(dst, src);
+    printf("copied string : %s\n", dst);
     
     return 0;
 }
