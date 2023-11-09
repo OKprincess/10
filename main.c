fbdf01fc9b860a893ba0db2d582a82f09817a673
OKprincess <144990952+OKprincess@users.noreply.github.com>
2023-11-09 16:07:39 +0900

    10_P5

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  10_P4
+//  10_P5
 //
 //  Created by ok sojoung on 2023/11/09.
 //
@@ -9,8 +9,22 @@
 #include <string.h>
 
 int main(void){
-    char str[30] = "happy C programming";
+    FILE* fp;
+    char str[100];
+    // 파일열기
+    fp = fopen("/Users/okso/sample.txt", "w");
+     
+    // 파일에 입력받은 값 쓰기
+    for(int i=0; i<3; i++)
+    {
+        printf("Input a word:");
+        scanf("%s", str);
+        
+        fprintf(fp, "%s\n", str);
+    }
     
-    printf("문자열\"%s\"의 길이 : %i\n", str, strlen(str));
+    // 파일 닫기
+    fclose(fp);
+    
     return 0;
 }
